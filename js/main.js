61150e6b23e1cdfa5f3dd793aadd7c821b3ddbea
Yair <yandrey2007@gmail.com>
2025-09-03 15:49:39 -0500

    Cree una funcion para el gif

--- js/main.js
+++ js/main.js
@@ -13,13 +13,19 @@ function estrella() {
     figure.style.clipPath =
     "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
 }
+function gif(){
+    figure.style.backgroundImage = "url('img/patito.gif')";
+    figure.style.backgroundSize = "cover"; 
+    figure.style.backgroundPosition = "center";
+}
+
 
 // Eventos
 btnCircle.onclick = circulo;
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("btnCircle").addEventListener('click', circulo)
     document.getElementById("btnStar").addEventListener('click', estrella)
-
+    document.getElementById("btnGif").addEventListener('click', gif)
     
 
 })
