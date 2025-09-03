73a15eb80837e083198789e41fe1a6871324928d
Yair <yandrey2007@gmail.com>
2025-09-03 15:43:44 -0500

    Cree una funcion para la estrella

--- js/main.js
+++ js/main.js
@@ -7,11 +7,19 @@ function circulo() {
   figure.style.borderRadius = "50%";  
    figure.style.background = "red";    
 }
+function estrella() {
+    figure.style.borderRadius = "0";
+    figure.style.background= "purple"
+    figure.style.clipPath =
+    "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+}
 
 // Eventos
 btnCircle.onclick = circulo;
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("btnCircle").addEventListener('click', circulo)
+    document.getElementById("btnStar").addEventListener('click', estrella)
+
     
 
 })
